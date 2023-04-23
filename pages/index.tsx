824b5738edd99a6b90511b36b53dbe102e484d99
sergiu <sergiumer@gmail.com>
2023-04-23 19:13:19 +0100

    Portfolio redesign.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,274 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from "react";
+import { useEffect } from "react";
+import Head from "./head";
+import Image from "next/image";
+import logo from "../public/s.png";
+import code from "../public/code.png";
+import design from "../public/design.png";
+import consulting from "../public/consulting.png";
+import profile from "../public/profile.png";
+import { BsFillMoonStarsFill, BsHeartFill } from "react-icons/bs";
+import { MdWbSunny } from "react-icons/md";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(true);
+  const baseClassName = "text-slate-700 cursor-pointer text-2xl";
+  const baseClassNameSun = "text-yellow-400 cursor-pointer text-3xl";
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Engineer", "Designer", "Consultant"];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(300);
+    }
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+    <div className={darkMode ? "dark" : ""}>
+      {/*THIS IS WHERE WE IDENTIFY HEAD TAG.*/}
+      <Head></Head>
+      {/* THIS DEALS WITH THE NAVBAR. */}
+      <nav className="py-6 md:px-24 lg:px-44 xl:px-60 2xl:px-80 px-10 bg-lightBackground dark:bg-backgroundColor">
+        <div className="p-5 rounded-2xl flex items-center justify-between bg-lightContrast dark:bg-contrastBackground transition ease-in-out duration-200 hover:shadow-lg hover:shadow-gray-300 dark:hover:shadow-zinc-800">
+          <a className="w-10 h-10 lg:ml-3" href="">
+            <Image src={logo} alt="Logo" />
           </a>
+          {/* BUTTONS FOR DARK MODE AND LETS CONNECT */}
+          <ul className="flex items-center lg:mr-3">
+            <li>
+              <BsFillMoonStarsFill
+                onClick={() => setDarkMode(!darkMode)}
+                className={`${baseClassName} ${darkMode ? "hidden" : ""}`}
+              />
+            </li>
+            <li>
+              <MdWbSunny
+                onClick={() => setDarkMode(!darkMode)}
+                className={`${baseClassNameSun} ${darkMode ? "" : "hidden"}`}
+              />
+            </li>
+            <li>
+              <a
+                className="transition-all duration-600 bg-gradient-to-br to-teal-600 via-blue-700 from-cyan-700 bg-size-200 text-white px-4 py-2 rounded-md ml-8 font-poppins hover:bg-right-bottom"
+                href="https://www.linkedin.com/in/sergiumereacre"
+              >
+                Let&apos;s Connect
+              </a>
+            </li>
+          </ul>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      </nav>
+      {/* THIS IS WHERE THE MAIN SECTION BEGINS */}
+      <main className="md:px-24 lg:px-44 xl:px-60 2xl:px-80 px-10 min-h-screen bg-lightBackground dark:bg-backgroundColor flex flex-col items-center gap-10">
+        {/* THIS SECTION DEALS WITH THE FIRST PART OF THE WEBPAGE. */}
+        <section className="bg-lightContrast dark:bg-contrastBackground rounded-2xl w-full xl:p-20 xl:gap-20 flex flex-col items-center md:flex-row p-12 justify-center gap-5 transition ease-in-out duration-200 hover:shadow-lg hover:shadow-gray-300 dark:hover:shadow-zinc-800">
+          {/* THIS IS THE SECTION WHERE THE CONTENT IS. */}
+          <Image
+            src={profile}
+            alt="Profile"
+            className="w-72 md:w-80 h-auto rounded-full"
+          ></Image>
+          <div>
+            <div className="text-center pb-5 md:pb-0">
+              <h2 className="text-5xl py-2 font-medium text-transparent bg-clip-text bg-gradient-to-l from-red-400 to-blue-500">
+                Sergiu Mereacre
+              </h2>
+              <h3 className="text-black text-2xl py-2 dark:text-cyan-200">
+                Developer and {text}
+                <div className="text-black dark:text-white inline animate-pulse">
+                  |
+                </div>
+              </h3>
+              <p className="text-md py-5 leading-7 text-gray-800 md:max-w-md mx-auto dark:text-gray-400">
+                I am a software engineer based in Ireland. I&apos;m passionate
+                about building and launching products that solve real world
+                problems. Computers are my{" "}
+                <span className="text-blue-500">passion</span>. I love to learn
+                new things and I am always looking for new{" "}
+                <span className="text-yellow-700 dark:text-yellow-500">
+                  opportunities
+                </span>
+                .
+              </p>
+            </div>
+            {/* THIS IS THE SECTION WHERE THE SOCIAL MEDIA ICONS ARE. */}
+            <ul className="text-5xl flex justify-center gap-10 text-gray-600">
+              <li>
+                <a
+                  className="hover:text-blue-700 hover:transition-all hover:duration-300"
+                  href="https://www.linkedin.com/in/sergiumereacre"
+                >
+                  <AiFillLinkedin />
+                </a>
+              </li>
+              <li>
+                <a
+                  className="hover:text-gray-800 hover:transition-all hover:duration-300"
+                  href="https://github.com/sergiumereacre"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
+        {/* THIS IS THE SECTION WHERE THE SECOND PART OF THE WEBPAGE IS. */}
+        <section className="bg-lightContrast dark:bg-contrastBackground rounded-2xl w-full flex flex-col items-center p-12 justify-center gap-5 transition ease-in-out duration-200 hover:shadow-lg hover:shadow-gray-300 dark:hover:shadow-zinc-800">
+          {/* THIS SECTION DEALS WITH THE PERSONAL EXPERIENCE PAROGRAPH. */}
+          <div>
+            <h3 className="text-2xl text-black dark:text-cyan-200">
+              Professional Experience
+            </h3>
+            <p className="text-md leading-7 text-gray-800 dark:text-gray-400">
+              From a very young age, I was inspired to design, create and mess
+              around with projects that found my interest. Learning new things
+              and building stuff from scratch is what I love to do. I have a{" "}
+              <span className="text-red-700 dark:text-red-400">
+                burning passion
+              </span>{" "}
+              for technology with a strong interest in software engineering and
+              I am always looking for new opportunities to learn and grow. This
+              is what led me to pursue a career in software engineering. I am
+              currently finishing my degree in Computer Science at the{" "}
+              <span className="text-green-700 dark:text-green-400 font-bold">
+                University of Limerick
+              </span>
+              .
+            </p>
+          </div>
+          {/* THIS SECTION DEALS WITH THE CARDS AT THE BOTTOM. */}
+          <div className="flex flex-col lg:flex-row flex-wrap gap-10 md:gap-5 justify-center">
+            <div>
+              <div className="flex flex-col gap-2 text-center py-10 px-5 rounded-3xl bg-lightBackground dark:bg-zinc-900 min-h-full min-w-fit md:max-w-sm lg:max-w-xs">
+                <div className="flex items-center justify-center">
+                  <Image
+                    src={code}
+                    width={100}
+                    height={100}
+                    alt="Coding Avatar"
+                  />
+                </div>
+                <h3 className="text-gray-800 text-lg font-medium pb-5 dark:text-cyan-200">
+                  Coding Projects
+                </h3>
+                <p className="text-gray-800 dark:text-gray-400">
+                  I have experience collaborating on team projects as well as
+                  working independently. Some have reached a small audience and
+                  others have had the potential to reach millions.
+                </p>
+                <h4 className="text-orange-600 dark:text-orange-700">
+                  Coding languages I&apos;ve used in the past.
+                </h4>
+                <div className="flex flex-col gap-1">
+                  <p className="text-gray-800 dark:text-gray-500">Python</p>
+                  <p className="text-gray-800 dark:text-gray-500">Java</p>
+                  <p className="text-gray-800 dark:text-gray-500">C++</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="flex flex-col gap-2 text-center py-10 px-5 rounded-3xl bg-lightBackground dark:bg-zinc-900 min-h-full min-w-fit md:max-w-sm lg:max-w-xs">
+                <div className="flex items-center justify-center">
+                  <Image
+                    src={design}
+                    width={100}
+                    height={100}
+                    alt="Design Avatar"
+                  />
+                </div>
+                <h3 className="text-gray-800 text-lg font-medium pb-5 dark:text-cyan-200">
+                  Beautiful Designs
+                </h3>
+                <p className="text-gray-800 dark:text-gray-400">
+                  I have collaborated with numerous clients to develop designs
+                  that are not only visually appealing but also meet their
+                  specific functional requirements and ideas.
+                </p>
+                <h4 className="text-orange-600 dark:text-orange-700">
+                  Design tools I use.
+                </h4>
+                <div className="flex flex-col gap-1">
+                  <p className="text-gray-800 dark:text-gray-500">Photoshop</p>
+                  <p className="text-gray-800 dark:text-gray-500">
+                    After Effects
+                  </p>
+                  <p className="text-gray-800 dark:text-gray-500">Figma</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="flex flex-col gap-2 text-center py-10 px-5 rounded-3xl bg-lightBackground dark:bg-zinc-900 min-h-full min-w-fit md:max-w-sm lg:max-w-xs">
+                <div className="flex items-center justify-center">
+                  <Image
+                    src={consulting}
+                    width={100}
+                    height={100}
+                    alt="Team Avatar"
+                  />
+                </div>
+                <h3 className="text-gray-800 text-lg font-medium pb-5 dark:text-cyan-200">
+                  Team Collaboration
+                </h3>
+                <p className="text-gray-800 dark:text-gray-400">
+                  I possess the proficiency of collaborating with teams of
+                  various sizes. From working in teams of two to collaborating
+                  with teams of 10 or more individuals during my time at Intel.
+                </p>
+                <h4 className="text-orange-600 dark:text-orange-700">
+                  Skills developed.
+                </h4>
+                <div className="flex flex-col gap-1">
+                  <p className="text-gray-800 dark:text-gray-500">Leadership</p>
+                  <p className="text-gray-800 dark:text-gray-500">
+                    Time Management
+                  </p>
+                  <p className="text-gray-800 dark:text-gray-500">
+                    Problem-solving
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="mb-5 pr-8 flex justify-center md:justify-end bg-lightContrast dark:bg-contrastBackground rounded-2xl w-full p-5 transition ease-in-out duration-200 hover:shadow-lg hover:shadow-gray-300 dark:hover:shadow-zinc-800">
+          <p className="flex gap-1 items-center text-gray-800 dark:text-gray-400">
+            Designed and implemented with{" "}
+            <BsHeartFill className="text-red-800" /> by Sergiu.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        </section>
+      </main>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,10 +8,26 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'contrastBackground': '#121214',
+        'backgroundColor': '#18181b',
+        'lightBackground': '#dff3f6',
+        'lightContrast': '#e2e2e2',
+      },
+      fontFamily: {
+        'poppins': "poppins",
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      backgroundSize: {
+        'size-200': '200% 200%',
+      },
+      backgroundPosition: {
+          'pos-0': '0% 0%',
+          'pos-100': '100% 100%',
       },
     },
   },
