09aef8d67669623e716fd27ce9a4ae7d7ad8bf0b
sergiu <sergiumer@gmail.com>
2023-08-04 23:31:48 +0100

    Updated to new app router and fixed scaling issues.

--- README.md
+++ README.md
@@ -10,6 +10,4 @@ npm run dev
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the project work in action.
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
-
 Pretty simple project portfolio.
---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import { Metadata } from "next";
+import './globals.css'
+
+
+export const metadata: Metadata = {
+    title: 'Sergiu Mereacre / Portfolio',
+    description: 'Portfolio website showing my work and my skills. Built using the latest Next.js and tailwindcss.',
+}
+
+export default function RootLayout({
+    // Layouts must accept a children prop.
+    // This will be populated with nested layouts or pages
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    )
+}
